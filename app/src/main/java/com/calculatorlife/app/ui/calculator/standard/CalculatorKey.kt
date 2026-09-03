8620c95f18931c23d099caa496eb33ddd3351d0e
Humon <humon@example.com>
2026-09-03 19:24:08 +0530

    Fix Compose compilation errors

--- app/src/main/java/com/calculatorlife/app/ui/calculator/standard/CalculatorKey.kt
+++ app/src/main/java/com/calculatorlife/app/ui/calculator/standard/CalculatorKey.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.ripple
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
